072d24a7e5eea8d90a07fb02b54e1e8f408f1d3f
adel <adelramdani27@gmail.com>
2025-02-19 14:16:34 +0100

    changed to react

--- frontend-template/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import user from "./slices/user";
-const store = configureStore({
-  reducer: {
-    user: user,
-  },
-});
-
-export default store;
