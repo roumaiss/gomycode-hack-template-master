60b7eaa65b9912b51000a3fa59ce7833fbba5fd4
Raid Mefti <raid.mefti@hotmail.com>
2025-02-19 19:50:42 +0100

    todo list done

--- frontend-template/src/app/layout.jsx
+++ frontend-template/src/app/layout.jsx
@@ -1,9 +1,15 @@
+"use client";
 import "./globals.css";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryclient = new QueryClient();
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                <QueryClientProvider client={queryclient}>
+                    {children}
+                </QueryClientProvider>
+            </body>
+        </html>
+    );
 }
--- frontend-template/src/app/page.jsx
+++ frontend-template/src/app/page.jsx
@@ -1,12 +1,13 @@
-import Navbar from "./components/Navbar";
-
+"use client";
+import Navbar from "../components/Navbar";
+import TodoList from "../components/TodoList";
 
 export default function Home() {
-  return (
-    <div>
-      <Navbar/>
-      <p>This is the Home page</p>
-
-    </div>
-  );
+    return (
+        <div>
+            <Navbar />
+            <p>This is the Home page</p>
+            <TodoList todos={[]} />
+        </div>
+    );
 }
